7676246020b3f8c888f80d63f0ba7f7ffaadd6ed
EdwardOakley <47759261+fatchild@users.noreply.github.com>
2021-12-30 16:37:42 +0000

    Port fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Load HTTP module
 const http = require("http");
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const express = require('express');
 const path = require('path');
